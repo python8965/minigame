158ae8c0aa32b6ddf9ffdc66798a5c8af3ad8c06
AKETON <python8965@gmail.com>
2022-09-05 10:19:40 +0900

    gogogo

---
+++ dodge/dodge.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+// ReSharper disable once CheckNamespace
+// ReSharper disable once InconsistentNaming
+// ReSharper disable once UnusedType.Global
+public class dodge : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+      
+    }
+}
