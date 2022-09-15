f2ebbae3d414a02e101128bb2feb98495414a146
AKETON <python8965@gmail.com>
2022-09-15 10:35:48 +0900

    3d player

--- games/first3d/player/First3DPlayer.cs
+++ games/first3d/player/First3DPlayer.cs
@@ -5,6 +5,8 @@ public class First3DPlayer : KinematicBody
 {
     [Export]
     public int Speed = 500;
+    public const float MinRotateY = -0.8f;
+    public const float MaxRotateY = 0.8f;
 
     public float Gravity = 490f;
     // Declare member variables here. Examples:
@@ -12,33 +14,56 @@ public class First3DPlayer : KinematicBody
     // private string b = "text";
     private Vector3 _velocity = Vector3.Zero; // The player's movement vector.
 
-    private float _rotateVelocity = 0;
+    private SpringArm _springArm;
+
+    
+    private Vector2 _mouseInputBuffer = Vector2.Zero;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        _springArm = GetNode<SpringArm>("SpringArm");
     }
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
+      GD.Print(_springArm.GlobalRotation);
+      Vector2 rotation = _mouseInputBuffer / 10;
+      _mouseInputBuffer -= rotation;
       
+      Rotate(Vector3.Up,Mathf.Deg2Rad(rotation.x/3) );
+      
+      if (_springArm.GlobalRotation.x > MaxRotateY && rotation.y < 0
+          )
+      {
+          
+      }else if (_springArm.GlobalRotation.x < MinRotateY && rotation.y > 0)
+      {
+          
+      }
+      else
+      {
+          _springArm.RotateObjectLocal(Vector3.Up, Mathf.Deg2Rad(rotation.y / 3));
+      }
+      
+      //Transform = Transform.Orthonormalized();
   }
 
   public override void _PhysicsProcess(float delta)
   {
       base._PhysicsProcess(delta);
-      GD.Print(_velocity);
       MoveAndSlide(_velocity.Rotated(new Vector3(0,1,0),Rotation.y)*delta);
-      RotateY(_rotateVelocity * delta);
+
+      
   }
+  
+  
   
   public override void _UnhandledInput(InputEvent @event)
   {
       base._UnhandledInput(@event);
         
       _velocity = Vector3.Zero;
-      _rotateVelocity = 0;
 
       if (Input.IsActionPressed("move_right"))
       {
@@ -70,18 +95,9 @@ public class First3DPlayer : KinematicBody
       if (@event is InputEventMouseMotion mouseMotion)
       {
           GD.Print();
+          _mouseInputBuffer += mouseMotion.Relative;
           
-          RotateY(Mathf.Deg2Rad(-mouseMotion.Relative.x/3));
-      }
-
-      if (Input.IsKeyPressed((int)KeyList.Q))
-      {
-          _rotateVelocity = 1;
-      }
-      
-      if (Input.IsKeyPressed((int)KeyList.E))
-      {
-          _rotateVelocity = -1;
+          
       }
   }
 }
